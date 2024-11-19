2b1a7679c2079373da25836ef6bd7cf014b83768
Reinhardt <56647015+Reinhardt254@users.noreply.github.com>
2024-11-19 15:17:50 +0300

    fixed auth errors

--- middleware.ts
+++ middleware.ts
@@ -2,10 +2,8 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
 
-export default clerkMiddleware(async (auth, request) => {
-  if (!isPublicRoute(request)) {
-    await auth().protect()
-  }
+export default clerkMiddleware((auth, req) => {
+  if (!isPublicRoute(req)) auth().protect()
 })
 
 export const config = {
